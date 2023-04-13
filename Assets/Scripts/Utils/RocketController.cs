83b6ccc764ff856bf11ae4fd5bf1800943aaf127
ClemiltonLobo <clemilton.lobo@gmail.com>
2023-04-12 23:22:35 -0300

    Criando animações, e ajustado os scripts.
    Criando animações para a chave, para o foguete e ajustando os scripts para que as mesma sejam executadas de acordo a história  do jogo.

--- Assets/Scripts/Utils/RocketController.cs
+++ Assets/Scripts/Utils/RocketController.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using Cinemachine;
 
 public class RocketController : MonoBehaviour
 {
     public GameObject messageText;
+    public GameObject messageRocketOn;
+    public GameObject messageVestKey;
     public KeyCode activationKey = KeyCode.E;
     public GameObject keyPrefab;
+    public GameObject playerObject;
+    public GameObject windowObject;
+    public GameObject insideRocketImage;
+    public CinemachineVirtualCamera CameraPlayer;
 
     private bool isPlayerNear = false;
     private bool hasKey = false;
+    private Animator animator;
+
+    [SerializeField] private Image keyImage;
 
     void Awake()
     {
         messageText.SetActive(false);
+        messageRocketOn.SetActive(false);
+        messageVestKey.SetActive(false);
+        keyImage.gameObject.SetActive(false);
+        animator = GetComponent<Animator>(); // pega o componente Animator do foguete
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -32,6 +47,8 @@ public class RocketController : MonoBehaviour
         {
             isPlayerNear = false;
             messageText.SetActive(false);
+            messageRocketOn.SetActive(false);
+            messageVestKey.SetActive(false);
         }
     }
 
@@ -42,11 +59,25 @@ public class RocketController : MonoBehaviour
             if (hasKey)
             {
                 // Inicia animação do foguete
-                Debug.Log("Foguete ligado!");
+                messageText.SetActive(false);
+                messageVestKey.SetActive(false);
+                //StartCoroutine(ActivateMessage(messageRocketOn, 2f));
+                keyImage.gameObject.SetActive(false);
+
+                // Ativa imagem da visão de dentro do foguete
+                insideRocketImage.SetActive(true);
+
+                // Desativa jogador da cena
+                playerObject.SetActive(false);
+                CameraPlayer.gameObject.SetActive(false);
+
+                // Chama a animação do foguete
+                animator.SetTrigger("Launch");
             }
             else
             {
-                Debug.Log("Colete a chave pra ligar o foguete");
+                messageText.SetActive(false);
+                messageVestKey.gameObject.SetActive(true);
             }
         }
     }
@@ -55,5 +86,12 @@ public class RocketController : MonoBehaviour
     {
         hasKey = true;
         Destroy(keyPrefab);
+        keyImage.gameObject.SetActive(true);
     }
+    /*IEnumerator ActivateMessage(GameObject messageObject, float duration)
+    {
+        messageObject.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        messageObject.SetActive(false);
+    }*/
 }
